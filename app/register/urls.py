efc6c363f2f1bea47c1b02e23b054f7e56388e8e
Eva Osyagina <eva.osyagina428@gmail.com>
2025-12-19 18:04:10 +0400

    добавления функции выхода из аккаунта

--- app/register/urls.py
+++ app/register/urls.py
@@ -5,4 +5,5 @@ app_name = 'register'
 urlpatterns = [
     path('login/', views.login_view,name='login'),
     path('register/', views.register,name='register'),
+    path('logout/', views.logout_view,name='logout'),
 ]
