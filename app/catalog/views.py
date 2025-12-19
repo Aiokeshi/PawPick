89a199016e8ad38cec559e98d6e5a8c25996d587
Eva Osyagina <eva.osyagina428@gmail.com>
2025-12-19 13:05:51 +0400

    корректный запуск нового приложения

--- app/catalog/views.py
+++ app/catalog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def catalog(request):
+    context= {
+        'title': 'Home',
+        'content': 'Главная страница'
+    }
+    return render(request, 'catalog/catalog.html', context)
+def product(request):
+    context= {
+        'title': 'Home',
+        'content': 'Главная страница'
+    }
+    return render(request, 'catalog/product.html', context)
