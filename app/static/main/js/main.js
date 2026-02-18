9d696e7f29d44e88fba89281b4ab85db4510e98d
Eva Osyagina <eva.osyagina428@gmail.com>
2026-02-18 22:57:55 +0400

    добавление маскота

---
+++ app/static/main/js/main.js
@@ -0,0 +1,13 @@
+const slides = document.querySelectorAll('.slider img');
+let i = 0;
+
+function show(n) {
+  slides[i].classList.remove('active');
+  i = (n + slides.length) % slides.length;
+  slides[i].classList.add('active');
+}
+
+document.getElementById('next').onclick = () => show(i + 1);
+document.getElementById('prev').onclick = () => show(i - 1);
+
+setInterval(() => show(i + 1), 3000);
