97ba58211525b6ce47f529452eb0e802412eb1e8
Eva <eva.osyagina428@gmail.com>
2026-05-09 14:32:41 +0400

    адаптив

--- app/catalog/models.py
+++ app/catalog/models.py
@@ -42,6 +42,7 @@ class Cards(models.Model):
     imageScnd = models.ImageField(upload_to='card_images', blank=True, null=True, verbose_name='Изображение')
     imageThrd = models.ImageField(upload_to='card_images', blank=True, null=True, verbose_name='Изображение')
     description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    health = models.TextField(blank=True, null=True, verbose_name='Здоровье')
 
     tags = models.ManyToManyField(
         Tag,
